751a47e3bec69a0ae5c2212311e43af20e90851b
XxCatxX <al364403@uji.es>
2019-10-26 01:44:46 +0200

    uwu

--- taskBot/tasBot.py
+++ taskBot/tasBot.py
@@ -8,8 +8,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
-
 update_id = None
 
 
@@ -50,5 +48,3 @@ def echo(bot):
             update.message.reply_text(update.message.text)
 
 
-if __name__ == '__main__':
-    main()
